22a273b85170eaff0b96e82dab3b5fa3a5b78f53
Hyojin Kwon <raccoonhj33@gmail.com>
2020-02-11 15:56:58 +0900

    200211 snippet_list view, used postman

--- config/snippets/serializers.py
+++ config/snippets/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('pk', 'title', 'code', 'linenos', 'language', 'style', 'created')
