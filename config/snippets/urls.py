2063e38f6bfddda5c8f695e3b143928094532c2a
Hyojin Kwon <raccoonhj33@gmail.com>
2020-02-11 17:01:55 +0900

    200211 completed tutorial3 - class-based view

--- config/snippets/urls.py
+++ config/snippets/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from . import apis
 
 app_name = 'snippets'
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail)
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+
+    # class-based view 사용하는 경우, as_view 함수 호출
+    # 클래스 자체를 전달하는 것이 아니라, as_view()로 함수를 만들어서 그 함수를 전달
+    # 클래스를 호출하면 인스턴스가 호출되고, 함수를 호출하면 해당값이 바로 호출
+    path('snippets/', apis.SnippetListCreateAPIView.as_view()),
+    path('snippets/<int:pk>/', apis.SnippetRetrieveUpdateDestroyAPIView.as_view())
 ]
