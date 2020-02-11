59df01045174268e5820a15390ac4cb7f3ed2e47
Hyojin Kwon <raccoonhj33@gmail.com>
2020-02-11 15:12:30 +0900

    200211 SnippetSerializer

--- config/snippets/serializers.py
+++ config/snippets/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
 
+from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
+
 
 class SnippetSerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField()
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            '''
+            instance.a = 'apple'
+            setattr(instance, 'a', 'apple')
+            -> 동적으로 값 할당
+            '''
+            setattr(instance, key, value)
+        instance.save()
+        return instance
