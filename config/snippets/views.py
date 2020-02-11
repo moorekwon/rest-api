894c5d3b0594da0aade52763314983e7506896a8
Hyojin Kwon <raccoonhj33@gmail.com>
2020-02-11 16:19:37 +0900

    200211 completed tutorial1 - snippet_detail get, patch, delete using postman

--- config/snippets/views.py
+++ config/snippets/views.py
@@ -1,8 +1,9 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import JSONParser
 
 from snippets.models import Snippet
@@ -23,3 +24,27 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.erros, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    # rest_framework가 갖고 있는 함수 -> 404 오류를 json 형태로 렌더링하여 돌려줌
+    snippet = get_object_or_404(Snippet, pk=pk)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PATCH':
+        data = JSONParser().parse(request)
+        # partial=True -> 주어진 부분만 update
+        serializer = SnippetSerializer(snippet, data=data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
