1aa334f36658d77d78294e782f03515f96107a51
Hyojin Kwon <raccoonhj33@gmail.com>
2020-02-11 14:29:18 +0900

    200211 used serializer

---
+++ config/snippets/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+class SnippetSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField()
